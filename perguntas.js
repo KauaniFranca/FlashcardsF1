c3d052b2ae5e1f574423c6353ab96def01069a4d
Kauani <148967475+KauaniFranca@users.noreply.github.com>
2024-09-25 21:49:53 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'History F1',
+    'Em que ano ocorreu a primeira corrida de Fórmula 1?',
+    ' A primeira corrida aconteceu em 13 de maio de 1950, no Grande Prêmio da Grã-Bretanha, em Silverstone.'
+)
+
+criaCartao(
+    'The Best Team ',
+    'Qual a melhor equipe de todos os tempos da F1?',
+    'Scuderia Ferrari, com o maoir número de vitórias além de ser a +incrível. ☆'
+)
+
+criaCartao(
+    'World Champion',
+    'Quem é o atual campeão mundial de Fórmula 1?',
+    'O neerlandês Max Verstappen, é o atual campeão da categoria, pela Red Bull.'
+)
+
+criaCartao(
+    'Driver Contract ',
+    'Qual é o piloto com o contrato mais extenso com a Scuderia Ferrari?',
+    'Charles Leclerc☆, que assinou um contrato em 2019 que se estende até 2024.'
+)
